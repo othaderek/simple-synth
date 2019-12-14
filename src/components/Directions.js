d0432309f83aa519fca239a81ea41c2f5ed12def
othaderek <otha.hernandez@gmail.com>
2019-12-13 21:09:29 -0500

    Fixed typo.

--- src/components/Directions.js
+++ src/components/Directions.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Directions() {
     return (
         <div>
-            <div>Press 'z' to lower the octave you are in and 'x' raise it!</div>
+            <div>Press 'z' to lower the octave you are in and 'x' to raise it!</div>
             <br></br>
             <div>Press 1 for sine, 2 for triangle, 3 for square</div>
         </div>
