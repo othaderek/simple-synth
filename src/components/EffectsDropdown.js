01086c172b337be8427bd54f18940842cecf108e
othaderek <otha.hernandez@gmail.com>
2020-04-03 14:54:14 -0400

    Changed back to select option tags.

--- src/components/EffectsDropdown.js
+++ src/components/EffectsDropdown.js
@@ -12,7 +12,7 @@ export default class EffectsDropdown extends Component {
         return (
             <div className="effects-container">
             <label>Effects</label>
-            <Dropdown>
+            {/* <Dropdown>
                 <Dropdown.Menu className="effects" onClick={this.handleChange} id="effect-select" name="effect">
                     <Dropdown.Item value={null} text="None"/>
                     <Dropdown.Item value="reverb" text="Reverb"/>
@@ -20,14 +20,14 @@ export default class EffectsDropdown extends Component {
                     <Dropdown.Item value="chorus" text="Chorus"/>
                     <Dropdown.Item value="bitcrusher" text="Bitcrusher"/>
                 </Dropdown.Menu>
-            </Dropdown>
-            {/* <select className="effects"onChange={this.handleChange} id="effect-select" name="effect">
+            </Dropdown> */}
+            <select className="effects"onChange={this.handleChange} id="effect-select" name="effect">
                 <option value={null}>None</option>
                 <option value="reverb">Reverb</option>
                 <option value="pingpong">Ping Pong Delay</option>
                 <option value="chorus">Chorus</option>
                 <option value="bitcrusher">Bitcrusher</option>
-            </select>   */}
+            </select>  
             </div>
 
         )
